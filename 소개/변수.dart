96d9c145140368e5d980632dd555c116344af0e5
opfic <opficdev@gmail.com>
2024-12-19 00:44:37 +0900

    변수.dart 추가

---
+++ 소개/변수.dart
@@ -0,0 +1,13 @@
+var name = "Voyager I";
+//  var name: String = "Voyager I"
+var year = 1977;
+//  var year: Int = 1977
+var antennaDiameter = 3.7;
+//  var antennaDiamter: Double = 3.7
+var flybyObjects = ["Jupiter", "Saturn", "Uranus", "Neptune"];  //  dart에서의 자료형은 List<String> 이다.
+//  var flybyObjects: [String] = ["Jupiter", "Saturn", "Uranus", "Neptune"];
+var image = { //  dart에서의 자료형은 Map<String, dynamic> 이다.
+  "tags": ["saturn"],
+  "url": "//path/to/saturn.jpg"
+};
+//  var image: [String, Any] = ["tags": ["saturn"], "url": "//path/to/saturn.jpg"]
