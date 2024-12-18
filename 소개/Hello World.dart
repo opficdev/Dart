8db2c7ccd6d5cd1b8dfd9abfaeaeb52d83f7947f
opfic <opficdev@gmail.com>
2024-12-18 23:59:26 +0900

    2024/12/18 Hello World 및 메인 함수

---
+++ 소개/Hello World.dart
@@ -0,0 +1,11 @@
+//  최상위 메인 함수
+//  Swift에서는 안써도 되지만 굳이 구현을 하자면
+// @main
+// struct Main {
+//   static func main() {
+//     print("Hello World")
+//   }
+// }
+void main() {
+    print("Hello World");
+}
