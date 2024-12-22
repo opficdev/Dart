3f6d8ed7119baa17781af7ecc4d39461b98df586
opfic <opficdev@gmail.com>
2024-12-23 01:25:26 +0900

    주석.dart 추가

---
+++ Syntax basics/주석.dart
@@ -0,0 +1,15 @@
+void main() {
+  // 한줄 주석
+
+  // 다행 주석
+  /*
+  Llama larry = Llama();
+  larry.feed();
+  larry.exercise();
+  larry.clean();
+  */
+
+  // 문서화 주석
+  /// 나 /** 를 사용하면 된다
+  /// 클래스, 메서드, 변수 등 코드 요소에 대한 설명을 작성하여 Dart 문서 생성 도구(dartdoc)에 포함되도록 함
+}
